38a1f0763f15a8a09e75c73e0135ff3cffbe715d
drothstein <drothstein00@gmail.com>
2022-05-04 01:51:48 -0400

    Inserting New Data Lab 4

--- Lab4/server.js
+++ Lab4/server.js
@@ -70,5 +70,55 @@ app.get("/cars/owner/:name", (req, res, next) => {
     });
 }); 
 
+app.post("/cars/new", (req, res, next) => {
+        var data   =   {
+            timestamp : req.body.timestamp,
+            email : req.body.email,
+            name : req.body.name,
+            year : req.body.year,
+            make : req.body.make,
+            model : req.body.model,
+            car_id : req.body.car_id,
+            judge_id : req.body.judge_id,
+            judge_name : req.body.judge_name,
+            racer_turbo : req.body.racer_turbo,
+            racer_supercharged : req.body.racer_supercharged,
+            racer_performance : req.body.racer_performance,
+            racer_horsepower : req.body.racer_horsepower,
+            car_overall : req.body.car_overall,
+            engine_modifications : req.body.engine_modifications,
+            engine_performance : req.body.engine_performance,
+            engine_chrome : req.body.engine_chrome,
+            engine_detailing : req.body.engine_detailing,
+            engine_cleanliness : req.body.engine_cleanliness,
+            body_frame_undercarriage : req.body.body_frame_undercarriage,
+            body_frame_suspension : req.body.body_frame_suspension,
+            body_frame_chrome : req.body.body_frame_chrome,
+            body_frame_detailing: req.body.body_frame_detailing,
+            body_frame_cleanliness: req.body.body_frame_cleanliness,
+            mods_paint: req.body.mods_paint,
+            mods_body: req.body.mods_body,
+            mods_wrap: req.body.mods_wrap,
+            mods_rims: req.body.mods_rims,
+            mods_interior: req.body.mods_interior,
+            mods_other: req.body.mods_other,
+            mods_ice: req.body.mods_ice,
+            mods_aftermarket: req.body.mods_aftermarket,
+            mods_wip: req.body.mods_wip,
+            mods_overall: req.body.mods_overall
+        }
+        var sql = 'INSERT INTO cars_table (Timestamp,Email,Name,Year,Make,Model,Car_ID,Judge_ID,Judge_Name,Racer_Turbo,Racer_Supercharged,Racer_Performance,Racer_Horsepower,Car_Overall,Engine_Modifications,Engine_Performance,Engine_Chrome,Engine_Detailing,Engine_Cleanliness,Body_Frame_Undercarriage,Body_Frame_Suspension,Body_Frame_Chrome,Body_Frame_Detailing,Body_Frame_Cleanliness,Mods_Paint,Mods_Body,Mods_Wrap,Mods_Rims,Mods_Interior,Mods_Other,Mods_ICE,Mods_Aftermarket,Mods_WIP,Mods_Overall) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+        var params = [data.timestamp, data.email, data.name, data.year, data.make, data.model, data.car_id, data.judge_id, data.judge_name, data.racer_turbo, data.racer_supercharged, data.racer_performance, data.racer_horsepower, data.car_overall, data.engine_modifications, data.engine_performance, data.engine_chrome, data.engine_detailing, data.engine_cleanliness, data.body_frame_undercarriage, data.body_frame_suspension, data.body_frame_chrome, data.body_frame_detailing, data.body_frame_cleanliness, data.mods_paint, data.mods_body, data.mods_wrap, data.mods_rims, data.mods_interior, data.mods_other, data.mods_ice, data.mods_aftermarket, data.mods_wip, data.mods_overall]
+        db.run(sql, params, function (err, result) {
+            if (err) {
+                res.status(400).json({ "error": err.message })
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": data,
+            })
+        })
+    })
 
 app.listen(3000)
