b30e939a1df85c16954e64dc46019b102b37d91b
drothstein <drothstein00@gmail.com>
2022-05-04 00:13:08 -0400

    Additonal updates to Lab4

--- Lab4/server.js
+++ Lab4/server.js
@@ -8,3 +8,21 @@ var bodyParser = require("body-parser");app.use(bodyParser.urlencoded({ extended
 app.get('/', (req, res) => {
     res.json('hello world')
     }) 
+
+
+    app.get("/cars", (req, res, next) => {
+        var sql = "select * from cars_table"
+        var params = []
+        db.all(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": rows
+            })
+        });
+    });
+
+    app.listen(3000)
